9fb8ee646936f800cd37d13c987bdda894992d71
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-05-10 17:01:31 +0900

    Add a configuration of the factory bot to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # See https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#rspec
+  config.include FactoryBot::Syntax::Methods
 end
