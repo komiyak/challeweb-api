c28c6041703a4994639bb877b64c6229f224b34b
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-05-13 16:46:58 +0900

    To gather a version number

--- api-spec/build_html.sh
+++ api-spec/build_html.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ ! -d ./build/swagger-codegen-3.0.8/ ]; then
+CODEGEN_V='3.0.8'
+
+if [ ! -d ./build/swagger-codegen-$CODEGEN_V/ ]; then
   rm -rf build/
 
   mkdir -p build/
-  wget -qO- https://github.com/swagger-api/swagger-codegen/archive/v3.0.8.tar.gz | tar zxvf - -C ./build
+  wget -qO- https://github.com/swagger-api/swagger-codegen/archive/v$CODEGEN_V.tar.gz | tar zxvf - -C ./build
 
   # Build the jar file.
-  pushd build/swagger-codegen-3.0.8/
+  pushd build/swagger-codegen-$CODEGEN_V/
   mvn clean package
   popd
 fi
 
 # Build a html from OpenAPI Spec.
-java -jar build/swagger-codegen-3.0.8/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i openapi.yaml -l html2 -o out/
+java -jar build/swagger-codegen-$CODEGEN_V/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -i openapi.yaml -l html2 -o out/
