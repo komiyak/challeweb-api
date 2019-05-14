6f6a41004e8ed28cd49df740c75f7b342e205ecf
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-05-14 14:18:03 +0900

    Implement the list of schools and years apis.

---
+++ spec/controllers/years_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe YearsController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+      [1, 2, 3].shuffle.each do |order_number|
+        create(:year, order_number: order_number)
+      end
+
+      get :index
+      expect(response).to have_http_status(:success)
+
+      body = parse_response_body
+      expect(body['data']).not_to be_empty
+      expect(body['data'].size).to eq(3)
+      expect(body['data'][0]['type']).to eq('year')
+      expect(body['data'][0]['attributes']['order_number']).to eq(1)
+      expect(body['data'][1]['attributes']['order_number']).to eq(2)
+      expect(body['data'][2]['attributes']['order_number']).to eq(3)
+    end
+  end
+end
